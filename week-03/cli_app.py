885e2182c97bb0193f97763ae627393d6c1e70e6
mustafaxln <mustafaeulun@gmail.com>
2026-06-04 10:46:26 +0300

    uygulamaya cikis ozelligi eklendi

--- week-03/cli_app.py
+++ week-03/cli_app.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+on_off = 0
+
+while on_off != 1:
+    print("1. Gorev ekle")
+    print("2. Gorevleri listele")
+    print("3. Gorev sil")
+    print("4. Cikis")
+    secim = int (input("Seciminizi yapin: "))
+    if secim == 4:
+        on_off = 1
+    
