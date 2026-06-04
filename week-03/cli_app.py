5976b11119484d08905d70b695e8bb99302b3e47
mustafaxln <mustafaeulun@gmail.com>
2026-06-04 11:24:44 +0300

    uygulamanin sonunda veda mesaji eklendi

--- week-03/cli_app.py
+++ week-03/cli_app.py
@@ -41,3 +41,5 @@ while on_off != 1:
                 print("--------------------------------")
                 print("Gorev silindi")
                 print("--------------------------------")
+
+print("Program sonlandirildi")
