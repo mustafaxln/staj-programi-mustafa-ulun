769a43ab8c622de5387c36e0c26b7b70a522f910
mustafaxln <mustafaeulun@gmail.com>
2026-06-04 11:03:08 +0300

    uygulamaya gorev listeleme ozelligi eklendi

--- week-03/cli_app.py
+++ week-03/cli_app.py
@@ -2,13 +2,26 @@ on_off = 0
 gorevler = []
 
 while on_off != 1:
+    print("--------------------------------")
+    print("--- CLI Uygulamasi ---")
     print("1. Gorev ekle")
     print("2. Gorevleri listele")
     print("3. Gorev sil")
     print("4. Cikis")
+    print("--------------------------------")
     secim = int (input("Seciminizi yapin: "))
     if secim == 4:
         on_off = 1
     elif secim == 1:
         gorev = input("Gorevi girin: ")
         gorevler.append(gorev)
+    elif secim == 2:
+        if gorevler == []:
+            print("--------------------------------")
+            print("Gorevler yok")
+            print("--------------------------------")
+        else:
+            print("--------------------------------")
+            for gorev in gorevler:
+                print(f"{gorevler.index(gorev) + 1}. {gorev}")
+            print("--------------------------------")
